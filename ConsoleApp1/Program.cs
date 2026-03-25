ef238d7926d330c0303fb15db84d0e70ad7dda71
Mujtaba Shakeel <mujtaba.shakeel@sofcom.net>
2026-03-25 09:33:17 +0500

    My First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student stud = new Student("Mujtaba", "22k5199", 8, "A");
+        stud.getStudent();
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        private string name;
+        private string id;
+        private int grade;
+        private string section;
+
+        public Student(string name, string id, int grade, string section)
+        {
+            this.name = name;
+            this.id = id;
+            this.grade = grade;
+            this.section = section;
+        }
+
+        public void getStudent()
+        {
+            Console.WriteLine("Student Name: " + name);
+            Console.WriteLine("Student Id: " + id);
+            Console.WriteLine("Student Grade: " + grade);
+            Console.WriteLine("Student Section: " + section);
+        }
+    }
+}
